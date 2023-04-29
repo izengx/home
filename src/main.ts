d5cdceb9fb82e10d105d654c25a97aa81ff4a1a1
cengxili <izengx@163.com>
2023-04-29 14:05:45 +0800

    feat: 首页设置时间展示

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus, {
+})
+app.use(store).use(router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h3>IZENGX</h3>
+    <h1 class="date">{{ state.date }}</h1>
+    <h1 class="time">{{ state.time }}</h1>
+    <div class="calendar">
+      <el-calendar v-model="state.fullTime" />
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script lang="ts" setup>
 
-export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-});
+import { defineComponent, onMounted, reactive, toRefs, ref } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import { ElMessage, ElForm } from 'element-plus'
+import moment from 'moment'
+
+
+interface StateType{
+  fullTime: any,
+  date: any,
+  time: any
+}
+
+const state = reactive<StateType>({
+  fullTime: new Date(),
+  date: moment(new Date()).format('YYYY-MM-DD'),
+  time: moment(new Date()).format('HH:mm:ss')
+})
+
+const rangeTime = () => {
+  setInterval(() => {
+    state.fullTime = new Date()
+    state.date = moment(new Date()).format('YYYY-MM-DD')
+    state.time = moment(new Date()).format('HH:mm:ss')
+  }, 1000)
+}
+
+rangeTime()
+
 </script>
+
+<style lang="scss">
+.home {
+  text-align: center;
+}
+.date {
+  font-size: 50px;
+}
+.time {
+  font-size: 80px;
+}
+// .calendar {
+//   height: 200px;
+// }
+// ::v-deep .el-calendar-table .el-calendar-day{
+//   height: 20px;
+// }
+</style>
